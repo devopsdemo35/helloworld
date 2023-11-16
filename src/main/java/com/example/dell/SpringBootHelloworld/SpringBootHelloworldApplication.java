591889224c880ee34460762adf608525d711a453
devopsdemo35 <devopsdemo35@gmail.com>
2023-11-16 10:23:55 +0530

    adding files

---
+++ src/main/java/com/example/dell/SpringBootHelloworld/SpringBootHelloworldApplication.java
@@ -0,0 +1,14 @@
+package com.example.dell.SpringBootHelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloworldApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/dell/SpringBootHelloworld/SpringBootHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dell.SpringBootHelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
